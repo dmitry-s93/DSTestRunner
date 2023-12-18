ec9708aebf05ec195034b1dfbcd13fc9fc4123c8
Dmitry Serov <dmitri.s93@gmail.com>
2023-12-18 18:35:06 +0300

    Update example-web-tests

--- example-web-tests/src/main/kotlin/tests/authentication/loginAndLogout/LoginAndLogoutTest.kt
+++ example-web-tests/src/main/kotlin/tests/authentication/loginAndLogout/LoginAndLogoutTest.kt
@@ -6,8 +6,8 @@ import action.actions.importUser
 import action.actions.setFieldValue
 import action.actions.web.closeBrowser
 import action.actions.web.openBrowser
-import authorizationPages
-import productsPages
+import pages.AuthorizationPageList.Companion.loginPage
+import pages.ProductsPageList.Companion.inventoryPage
 import reporter.allure.SeverityLevel
 import reporter.allure.allure
 import test.TestBuilder
@@ -21,18 +21,18 @@ class LoginAndLogoutTest : TestBuilder("TEST_01", "Login and logout") {
             severity(SeverityLevel.BLOCKER)
         }
         before {
-            step("01_before", openBrowser(authorizationPages.login))
-            step("02_before", checkLoadPage(authorizationPages.login))
+            step("01_before", openBrowser(loginPage))
+            step("02_before", checkLoadPage(loginPage))
             step("03_before", importUser("StandardUser", "user"))
         }
         steps {
             step("01", setFieldValue("UsernameEdit", "{user_login}"))
             step("02", setFieldValue("PasswordEdit", "{user_password}"))
             step("03", click("LoginButton"))
-            step("04", checkLoadPage(productsPages.inventory))
+            step("04", checkLoadPage(inventoryPage))
             step("05", click("BurgerMenuButton"))
             step("06", click("LogoutLink"))
-            step("07", checkLoadPage(authorizationPages.login))
+            step("07", checkLoadPage(loginPage))
         }
         after {
             step("01_after", closeBrowser())
